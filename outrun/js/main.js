755811ea97577ce21afb29660d921c086295e5e3
fungi <67556111+fugni@users.noreply.github.com>
2023-06-19 20:07:45 +0200

    car kinda worky

--- outrun/js/main.js
+++ outrun/js/main.js
@@ -8,14 +8,14 @@ var createScene = function () {
 var scene = new BABYLON.Scene(engine);
 
 // camera
-var camera = new BABYLON.FreeCamera("camera", new BABYLON.Vector3(0, 10, 0), scene);
-camera.attachControl(canvas, true);
+var camera = new BABYLON.FreeCamera("camera", new BABYLON.Vector3(0, 10, -20), scene);
+camera.attachControl(canvas, false);
 
 // light
 var light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), scene);
 light.intensity = 0.7;
 
-// ground
+// grid
 var ground = BABYLON.MeshBuilder.CreateGround("ground", {width: 80, height: 80}, scene);
 
 var groundMaterial = new BABYLON.GridMaterial("groundMaterial");
@@ -27,13 +27,54 @@ groundMaterial.opacity = 0.98;
 
 ground.material = groundMaterial;
 
-BABYLON.SceneLoader.Append("./", "duck.gltf", scene, function (scene) {
-    // do something with the scene
-  });
+// car
+var countach;
+BABYLON.SceneLoader.ImportMesh("", "./assets/countach_stylized_low-fi/", "scene.gltf", scene, function (scene) {
+    countach = scene[0];
+    
+    countach.scaling = new BABYLON.Vector3(10, 10, 10);
+    countach.rotation = new BABYLON.Vector3(0, Math.PI / -2, 0); // Example rotation
+});
+
+// shitty fix to make code not run before car is loaded
+var characterIsLoaded = false;
+setTimeout(() => {
+    characterIsLoaded = true;
+}, 30);
+
+var inputMap = {};
+scene.onKeyboardObservable.add((kbInfo) => {
+    if (kbInfo.type == BABYLON.KeyboardEventTypes.KEYDOWN) {
+        inputMap[kbInfo.event.key] = kbInfo.event.type == "keydown";
+    } else if (kbInfo.type == BABYLON.KeyboardEventTypes.KEYUP) {
+        inputMap[kbInfo.event.key] = kbInfo.event.type == "keydown";
+    }
+})
 
 engine.runRenderLoop(function () {
-
+    if (inputMap["w"]) {
+        countach.position.z += 0.1;
+        // countach.rotation.y = Math.PI / -2;
+    }
+    if (inputMap["a"]) {
+        countach.position.x -= 0.1;
+        // countach.rotation.y = Math.PI;
+    }
+    if (inputMap["s"]) {
+        countach.position.z -= 0.1;
+        // countach.rotation.y = Math.PI / 2;
+    }
+    if (inputMap["d"]) {
+        countach.position.x += 0.1;
+        // countach.rotation.y = Math.PI * 2;
+    }
+    if (characterIsLoaded) {
+        camera.position.copyFrom(countach.position);
+        // camera.setTarget(countach.position);
+    }
 }) 
+
+
 
 return scene;
 };
